d65eb5edaf1c13687b0931730577d67667544290
tediko <tediko123@wp.pl>
2020-12-05 11:10:11 +0100

    Added observer for lazy animations

---
+++ js/lazyLoad.js
@@ -0,0 +1,18 @@
+const sections = document.querySelectorAll('[data-anim]');
+
+const options = {
+    rootMargin: '0px',
+    threshold: 0.6
+}
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+        }
+    })
+}, options)
+
+export default sections.forEach(section => {
+    observer.observe(section);
+})
